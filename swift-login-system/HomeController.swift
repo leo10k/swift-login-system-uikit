4766fdc955e0f00cba75a8a2cca643b374a615ed
leo10k <leozinhomacedo41@gmail.com>
2024-01-12 18:21:30 -0300

    crating the setup to build the app

---
+++ swift-login-system/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  swift-login-system
+//
+//  Created by Leonardo Macedo on 12/01/24.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+    }
+    
+}
