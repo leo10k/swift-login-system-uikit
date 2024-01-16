f4465443a3d5ff8106ae745ce5361cc0235b7b59
leo10k <leozinhomacedo41@gmail.com>
2024-01-16 10:18:52 -0300

    modifing the ForgotPasswordController and HomeController

--- swift-login-system/HomeController.swift
+++ swift-login-system/HomeController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class HomeController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        label.text = "Loading..."
+        label.numberOfLines = 2
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemRed
+        self.setupUI()
+        
+        self.label.text = "Leo here\ncode is life"
+    }
+    
+    private func setupUI() {
+        self.view.backgroundColor = .systemBackground
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(didTapLogo))
+        
+        self.view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+    
+    @objc private func didTapLogo() {
+        
     }
     
 }
